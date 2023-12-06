a8d6079d3d0077ec19f70ea08c1ec71a6aaf61b1
thanhhai45 <thanhhai82451495@gmail.com>
2023-12-06 23:29:53 +0700

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jpg|png|jpeg)\z/i,
+    message: 'must be a URL for GIF, JPG, JPEG or PNG image'
+  }
 end
